d80ef8ae2cc3dfa829dff4a8a951f804c35c57f9
Anton Zaets <azaets.mail@gmail.com>
2019-09-11 22:30:23 +0300

    more comments and docs

--- redirect_resolver.py
+++ redirect_resolver.py
@@ -1,15 +1,25 @@
 #!/bin/python3
+"""Module and application for finding the target url in chain of redirects"""
+
 import argparse
 import urllib.request
 
 
 class RedirectResolver:
+    """The class that delivers functionality for url resolving"""
 
     def __init__(self, max_redirects=3, ignore_unlimited=False):
+        """
+            :param max_redirects: maximum number of redirects before
+                                  rising TooManyRedirectsError
+            :param ignore_unlimited: don't rise UnlimitedContentError if
+                                     chunked response is detected
+        """
         self._max_redirects = max_redirects
         self._ignore_unlimited = ignore_unlimited
 
     def resolve(self, url):
+        """finds a target url in the chain of redirects for url"""
         handlers = [_RedirectHandler(url, self._max_redirects)]
         if not self._ignore_unlimited:
             handlers.append(_UnlimitedContentProcessor())
@@ -20,31 +30,43 @@ class RedirectResolver:
 
 
 class TooManyRedirectsError(Exception):
+    """It's raised if too many redirects occured for url"""
     pass
 
 
 class CyclicRedirectError(Exception):
+    """It's raised if the cycle is detected for url"""
     pass
 
 
 class UnlimitedContentError(Exception):
+    """It's raised if the chunked response is detected"""
     pass
 
 
 class _RedirectHandler(urllib.request.HTTPRedirectHandler):
+    """Overrided handler for redirections"""
 
     def __init__(self, url, max_redirects):
+        """
+            :param url: url for resolving
+            :param max_redirects: maximum number of redirects
+                                  during resolving of the url
+        """
         self._max_redirects = max_redirects
         self._redirect_counter = 0
         self._visited = set([url])
         super().__init__()
 
     def redirect_request(self, req, fp, code, msg, hdrs, newurl):
-        '''
+        """
             That function reimplements features of HTTPRedirectHandler
-            for loop detection since interface for controlling detection
-            behavious isn't documented and could be treated as private
-        '''
+            for loop detection due too several reasons:
+                - interface for controlling detection behaviour isn't
+                  documented and variables for controlling that are stored
+                  as static members of HTTPRedirectHandler;
+                - support for continue with  'HEAD' after redirection
+        """
         self._redirect_counter += 1
         if self._redirect_counter > self._max_redirects:
             raise TooManyRedirectsError()
@@ -57,6 +79,9 @@ class _RedirectHandler(urllib.request.HTTPRedirectHandler):
         return new_request
 
     def http_error_308(self, req, fp, code, msg, hdrs):
+        """Method for supporting the redirection after getting
+           the 308 from the server
+        """
         return super().http_error_307(req, fp, 307, msg, hdrs)
 
 
@@ -66,6 +91,8 @@ class _UnlimitedContentProcessor(urllib.request.HTTPErrorProcessor):
         super().__init__()
 
     def http_response(self, req, resp):
+        """Method raises if response is 'chunked'
+        """
         transfer_value = resp.getheader('transfer-encoding')
         if transfer_value and 'chunked' in transfer_value:
             raise UnlimitedContentError()
@@ -75,10 +102,11 @@ class _UnlimitedContentProcessor(urllib.request.HTTPErrorProcessor):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--max-redirects', type=int, default=5,
-        help='Maximum number of redirects')
+                        help='Maximum number of redirects')
     parser.add_argument('--ignore-unlimited-content',
-        action='store_true', default=False,
-        help='Don\'t rise an exception if content may have unlimited size')
+                        action='store_true', default=False,
+                        help='''Don\'t rise an exception if content
+                                    may have unlimited size''')
     parser.add_argument('url', type=str, help='URL for resolving')
     args = parser.parse_args()
     resolver = RedirectResolver(
--- tests/redirect_server.py
+++ tests/redirect_server.py
@@ -6,10 +6,13 @@ from threading import Thread
 
 
 class RedirectServer(HTTPServer):
+    """Server for testing the RedirectResolver"""
 
     def __init__(self):
-        super().__init__(('127.0.0.1', 8080), RedirectRequestHandler)
+        super().__init__(('127.0.0.1', 8080), _RedirectRequestHandler)
         self._thread = Thread(target=self.serve_forever)
+        # that variable maps paths to generators of a new paths, codes
+        # and headers
         self._redirects = {}
 
     def start(self):
@@ -20,37 +23,51 @@ class RedirectServer(HTTPServer):
         self._thread.join()
 
     def many_redirects(self, code, num):
+        """Creates a multiple redirect
+            :param code: response code for redirect (30x)
+            :param num: number of redirects (more than 0)
+            :return: tuple like (url_for_resolving, expected_url)
+        """
+        assert code in self.get_redirect_codes()
         assert num > 0, "number of redirects should be greater than 0"
         path = self._random_path()
         target_path = self._random_path()
-        print('generator of many redirects for path {}'.format(path))
         self._redirects[path] = self._gen_many_redirects(
             path, target_path, code, num)
         return (self._build_url(path), self._build_url(target_path))
 
     def cyclic_redirect(self, code, num):
+        """Creates a cyclic redirect
+            :param code: response code for redirect (30x)
+            :param num: number of redirects (more than 0)
+            :return: url for resolving
+        """
+        assert code in self.get_redirect_codes()
         assert num > 0, "number of redirects should be greater than 0"
         path = self._random_path()
-        print('generator of cyclic redirects for path {}'.format(path))
         self._redirects[path] = self._gen_cycle_redirects(path, code, num)
         return self._build_url(path)
 
     def no_redirects(self):
+        """Create url without any redirect"""
         path = self._random_path()
         self._redirects[path] = itertools.cycle([(200, [])])
         return self._build_url(path)
 
     def unlimited_content(self):
+        """Create url with chunked response"""
         path = self._random_path()
         chunked_headers = [('Transfer-Encoding', 'gzip, chunked')]
         self._redirects[path] = itertools.cycle([(200, chunked_headers)])
         return self._build_url(path)
 
     def get_redirect_info(self, path):
-        # it's better to explicitly crash for unexpected path,
-        # since RedirectServer is used for testing purpose
-        assert path in self._redirects, \
-               'path {} not in {}'.format(path, self._redirects.keys())
+        """
+            :return: information about redirect for path,
+                     tuple like (response_code, list_of_headers)
+        """
+        assert path in self._redirects,\
+            'path {} not in {}'.format(path, self._redirects.keys())
         try:
             return next(self._redirects[path])
         except StopIteration:
@@ -58,6 +75,7 @@ class RedirectServer(HTTPServer):
 
     @staticmethod
     def get_redirect_codes():
+        """HTTP codes that should be trated as redirection"""
         return [301, 302, 303, 307, 308]
 
     def _gen_many_redirects(self, init_path, target_path, code, num):
@@ -66,9 +84,11 @@ class RedirectServer(HTTPServer):
             + [(code, target_path)]
         for new_code, new_path in paths:
             generator = self._redirects[previous_path]
+            # unregister/register generator for a old/new path
             del self._redirects[previous_path]
             self._redirects[new_path] = generator
             previous_path = new_path
+
             yield (new_code, [('Location', self._build_url(new_path))])
 
     def _gen_cycle_redirects(self, init_path, code, num):
@@ -77,6 +97,7 @@ class RedirectServer(HTTPServer):
         previous_path = init_path
         for new_path in itertools.cycle(paths):
             generator = self._redirects[previous_path]
+            # unregister/register generator for a old/new path
             del self._redirects[previous_path]
             self._redirects[new_path] = generator
             previous_path = new_path
@@ -90,7 +111,9 @@ class RedirectServer(HTTPServer):
         return urljoin('http://{}:{}/'.format(address, port), path)
 
 
-class RedirectRequestHandler(BaseHTTPRequestHandler):
+class _RedirectRequestHandler(BaseHTTPRequestHandler):
+    """Class implements response for HEAD request with
+       optional redirects"""
     def __init__(self, request, client_address, server):
         super().__init__(request, client_address, server)
 

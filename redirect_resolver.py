8db93b65126fc85dda971d022c28ab4561ed6215
Anton Zaets <azaets.mail@gmail.com>
2019-09-11 10:33:13 +0300

    limit for number of redirects work

--- redirect_resolver.py
+++ redirect_resolver.py
@@ -1,13 +1,15 @@
 #!/bin/python3
-from urllib import request
+import urllib.request
 
 class RedirectResolver:
 
     def __init__(self, max_redirects=3):
-        pass
+        self._max_redirects = max_redirects
 
     def resolve(self, url):
-        with request.urlopen(url) as http_response:
+        opener = urllib.request.build_opener(
+            _RedirectHandler(self._max_redirects))
+        with opener.open(url) as http_response:
             print('GET {} - url: {} info: {} code {}'.format(
                 url,
                 http_response.geturl(),
@@ -20,6 +22,18 @@ class TooManyRedirectsError(Exception):
 
 class CyclicRedirectError(Exception):
     pass
+
+class _RedirectHandler(urllib.request.HTTPRedirectHandler):
+
+    def __init__(self, max_redirects):
+        self._max_redirects = max_redirects
+        self._redirect_counter = 0
+
+    def redirect_request(self, req, fp, code, msg, hdrs, newurl):
+        self._redirect_counter += 1
+        if self._redirect_counter > self._max_redirects:
+            raise TooManyRedirectsError()
+        return super().redirect_request(req, fp, code, msg, hdrs, newurl)
 
 if __name__ == '__main__':
     print('hello')
